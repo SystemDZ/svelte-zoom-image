d35f689b49c9e7eed78500ba154d95be8e2a3eba
ASHLEYFURNITURE\mfellak <mfellak@ashleyfurniture.com>
2021-02-25 10:01:34 -0500

    small text update

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,43 @@
 <script>
+	export let credentials;
+
 	let m = { x: 0, y: 0 };
-	let normalImage = 'https://i.ibb.co/Ph9MCB2/template.png'
-	let zoomedImage = 'https://i.ibb.co/vhL5kH2/image-14.png'
+	let normalImage = "https://i.ibb.co/Ph9MCB2/template.png";
+	let zoomedImage = "https://i.ibb.co/vhL5kH2/image-14.png";
 	let bg = normalImage;
 	function handleMousemove(event) {
-		m.x = -(event.clientX);
-		m.y = -(event.clientY);
-		bg = zoomedImage
+		m.x = -event.clientX;
+		m.y = -event.clientY;
+		bg = zoomedImage;
 	}
 	function handleMouseOut(event) {
 		m.x = 0;
 		m.y = 0;
-		bg = normalImage
+		bg = normalImage;
 	}
 </script>
 
 <div class="container">
-     <div class="zoom-handler"
-					on:mousemove={handleMousemove}
-					on:mouseout={handleMouseOut}
-					on:click
-					style="--bgx:{m.x}px; --bgy:{m.y}px;--bg: url({bg})">
-     </div>
+	<div
+		class="zoom-handler"
+		on:mousemove={handleMousemove}
+		on:mouseout={handleMouseOut}
+		on:click
+		style="--bgx:{m.x}px; --bgy:{m.y}px;--bg: url({bg})"
+	/>
 </div>
+<p>
+	Developed by <a
+		href="http://fellak.com"
+		target="_blank"
+		title="Mohammed Fellak">{credentials}</a
+	>
+</p>
 
 <style>
 	.container {
-		 max-width: 670px;
-		 height: 377px;
+		max-width: 670px;
+		height: 377px;
 	}
 	.zoom-handler {
 		max-width: 100%;
